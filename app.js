3b7595a00ea79a3abbbc6c186cdfaf7c066598a7
Kachev032 <akkachev@gmail.com>
2023-12-07 18:53:25 +0200

    updated

--- app.js
+++ app.js
@@ -1,37 +1,3 @@
-//Swiper.js code for the projects card swipe effect//
-const swiper = new Swiper(".swiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: 2,
-  speed: 600,
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 100,
-    modifier: 3,
-    slideShadows: true,
-  },
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: false,
-  },
-});
-
-//Clicking the cat to show the hidden message//
-const clickableImg = document.getElementById("clickable-img");
-const hiddenMsg = document.getElementById("hidden-message");
-
-let messageVisible = false;
-
-clickableImg.addEventListener("click", function () {
-  if (!messageVisible) {
-    hiddenMsg.style.display = "block";
-    messageVisible = true;
-  }
-});
-
 ////// Toggle Navbar/////
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
